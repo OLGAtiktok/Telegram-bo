b2c52489afbfdd782be473f45d0bc51059ac1761
OLGAtiktok <Kulevicholga@gmail.com>
2025-04-30 14:33:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-
-import os
 import telebot
 
 TOKEN = "8152614638:AAEtIoLJbEmtwLy5rwEZaLaxI01nKIi09q4"
@@ -7,10 +5,10 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Привет! Я бот и я работаю!")
+    bot.send_message(message.chat.id, "Привет! Я живой!")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.send_message(message.chat.id, "Ты сказал: " + message.text)
+    bot.send_message(message.chat.id, f"Ты сказал: {message.text}")
 
 bot.polling()
